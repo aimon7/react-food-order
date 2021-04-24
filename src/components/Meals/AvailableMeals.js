04e1d8eb664edaed914771703792184854b192ed
aimon7 <air.ioannis@gmail.com>
2021-04-24 23:05:00 +0300

    Add individual MealItem.js component

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,6 @@
 import classes from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -29,14 +31,16 @@ const DUMMY_MEALS = [
 
 function AvailableMeals() {
     const mealsList = DUMMY_MEALS.map(meal => {
-        return <li>{meal.name}</li>
+        return <MealItem key={meal.id} name={meal.name} price={meal.price} description={meal.description} />
     });
 
     return (
         <section className={classes.meals}>
-            <ul>
-                {mealsList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,9 @@
+import classes from './MealItemForm.module.css';
+
+function MealItemForm(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default MealItemForm;
